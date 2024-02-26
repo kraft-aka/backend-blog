265adbcf3de00229c1896b3fda5ebcd3bcc2757d
kubat <kubat.aiupov@gmail.com>
2024-02-26 13:36:57 +0100

    add edit getBlogs by userId

--- routes/comment.js
+++ routes/comment.js
@@ -14,7 +14,7 @@ const {
 
 // routers
 router.post("/blogs/comment/:id", verifyUser, newComment); // privat route with url endpoint, uses POST HTTP method, creates a comment
-router.get("/comments", getAllComments); // public route with url endpoint, uses GET HTTP method, fetches all comments
+router.get("/comments/:blogId", getAllComments); // public route with url endpoint, uses GET HTTP method, fetches all comments
 router.put("/comments/:id", verifyUser, editComment); // privat route with url endpoint, uses PUT HTTP method, updates the comment
 router.delete("/comments/deletecomment/:id", verifyUser, deleteComment); // privat route with url endpoint, uses DELETE HHTP method, deletes comment
 router.put("/comments/addlike/:id", verifyUser, addLiketoComment); // privat route with url endpoint, uses PUT HHTP method, adds like to comment
